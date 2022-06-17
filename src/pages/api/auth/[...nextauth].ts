3dc6e44a1e6075cdb48d85e045348b0ff73885be
Michal Bachraty <misko.bachraty@gmail.com>
2022-06-17 11:51:51 +0200

    get department microsoft graph

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import NextAuth from "next-auth/next";
 import AzureADProvider from "next-auth/providers/azure-ad";
 
@@ -7,11 +8,24 @@ export default NextAuth({
       clientId: process.env.AZURE_AD_CLIENT_ID,
       clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
       tenantId: process.env.AZURE_AD_TENANT_ID,
+      authorization: { params: { scope: "openid email profile User.Read" } },
     }),
   ],
   callbacks: {
     async signIn({ user, account, profile, email, credentials }) {
-      console.log(account, profile, credentials);
+      //console.log(account, profile, credentials);
+      let department;
+      await axios
+        .get("https://graph.microsoft.com/v1.0/me?$select=department", {
+          headers: { Authorization: `Bearer ${account.access_token}` },
+        })
+        .then((response) => {
+          department = response.data.department;
+        })
+        .catch((error) => {
+          console.log("error response", error.response);
+        });
+      console.log("department", department);
       return true;
     },
   },
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -4,6 +4,7 @@ declare global {
       AZURE_AD_CLIENT_ID: string;
       AZURE_AD_CLIENT_SECRET: string;
       AZURE_AD_TENANT_ID: string;
+      NEXTAUTH_SECRET: string;
     }
   }
 }
