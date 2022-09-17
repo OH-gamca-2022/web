96b220674d7b08ac7070f14721e32d6c78c9f73b
Michal Bachraty <misko.bachraty@gmail.com>
2022-09-17 20:53:24 +0200

    google redirect uri change

--- src/utils/google-signin.ts
+++ src/utils/google-signin.ts
@@ -4,7 +4,7 @@ import fs from "fs";
 export const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  "http://localhost:3000/api/google-oauth-callback"
+  `${process.env.BASE_URL}/api/google-oauth-callback`
 );
 
 export const generateURL = () => {
