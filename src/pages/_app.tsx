dda1375d7647ef113effc44729a2fabf50c04749
Michal Bachraty <misko.bachraty@gmail.com>
2022-10-03 21:51:21 +0200

    favicon change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -199,7 +199,23 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     <>
       <Head>
         <title>OH Hokus Pokus</title>
-        <link rel="shortcut icon" href="/logo.svg" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <meta name="theme-color" content="#000" />
       </Head>
       <SessionProvider session={session} basePath="/api/auth">
